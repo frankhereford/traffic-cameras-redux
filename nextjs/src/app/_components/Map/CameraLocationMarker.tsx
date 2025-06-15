46c5e65359ccd13de2ca0cb821b254db539288ea
Frank Hereford <github@frankhereford.com>
2025-06-15 16:16:58 +0000

    Add custom marker icon for CameraLocationMarker component
    This commit introduces a custom marker icon for the CameraLocationMarker component, enhancing the visual representation of camera locations on the map. The marker icon is defined with specific properties such as color, opacity, and scale, and is now passed to the MarkerF component for rendering.

--- nextjs/src/app/_components/Map/CameraLocationMarker.tsx
+++ nextjs/src/app/_components/Map/CameraLocationMarker.tsx
@@ -8,6 +8,15 @@ interface CameraLocationMarkerProps {
 export default function CameraLocationMarker({
   socrataData,
 }: CameraLocationMarkerProps) {
+  const markerIcon = {
+    path: "M-2,0a2,2 0 1,0 4,0a2,2 0 1,0 -4,0",
+    fillColor: "blue",
+    fillOpacity: 0.9,
+    strokeColor: "black",
+    strokeWeight: 1.5,
+    scale: 3,
+  }
+
   return (
     <>
       {socrataData.map((data) => {
@@ -16,7 +25,7 @@ export default function CameraLocationMarker({
             lat: data.location.coordinates[1]!,
             lng: data.location.coordinates[0]!,
           }
-          return <MarkerF key={data.camera_id} position={position} />
+          return <MarkerF key={data.camera_id} position={position} icon={markerIcon} />
         }
         return null
       })}
