3c1a04c8da97b3c3bff2555c11439dec748ca727
Frank Hereford <github@frankhereford.com>
2025-06-15 15:35:48 +0000

    Add Google Maps integration with @react-google-maps/api
    This commit introduces the @react-google-maps/api dependency to the Next.js project, allowing for Google Maps functionality. A new Map component is created to render a basic map interface. Additionally, the Home page is updated to include the Map component, replacing the previous layout with links and greeting messages.

---
+++ nextjs/src/app/_components/Map/Map.tsx
@@ -0,0 +1,16 @@
+import { GoogleMap, useJsApiLoader, OverlayView } from "@react-google-maps/api"
+
+
+interface MapProps {
+
+}
+
+function Map({ }: MapProps) {
+return (
+    <div>
+        <h1>Map</h1>
+    </div>
+)
+}
+
+export default Map;
