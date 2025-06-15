8d9794eec5045329d1cc5ac0bc7d920cf314ae87
Frank Hereford <github@frankhereford.com>
2025-06-15 16:29:15 +0000

    Update Map components to integrate @vis.gl/react-google-maps and enhance zoom functionality
    This commit adds the @vis.gl/react-google-maps dependency to the project, allowing for improved map features. The CameraLocationMarker component is updated to accept a zoom prop, and the Map component now manages zoom state and updates accordingly. Additionally, the marker icon is adjusted for better visual representation.

--- nextjs/src/app/_components/Map/CameraLocationMarker.tsx
+++ nextjs/src/app/_components/Map/CameraLocationMarker.tsx
@@ -1,19 +1,31 @@
 import type { SocrataData } from "~/app/_hooks/useSocrataData"
-import { MarkerF } from "@react-google-maps/api"
+import { Marker } from "@react-google-maps/api"
+
 
 interface CameraLocationMarkerProps {
   socrataData: SocrataData[]
+  zoom: number
 }
 
 export default function CameraLocationMarker({
   socrataData,
+  zoom,
 }: CameraLocationMarkerProps) {
+  const getScale = (zoom: number) => {
+    if (zoom < 10) return 1;
+    if (zoom < 12) return 2;
+    if (zoom < 15) return 3;
+    if (zoom < 18) return 4;
+    return 5;
+  }
+
   const markerIcon = {
     path: "M-2,0a2,2 0 1,0 4,0a2,2 0 1,0 -4,0",
     fillColor: "blue",
     fillOpacity: 0.9,
     strokeColor: "black",
     strokeWeight: 1.5,
+    // scale: getScale(zoom),
     scale: 3,
   }
 
@@ -25,7 +37,7 @@ export default function CameraLocationMarker({
             lat: data.location.coordinates[1]!,
             lng: data.location.coordinates[0]!,
           }
-          return <MarkerF key={data.camera_id} position={position} icon={markerIcon} />
+          return <Marker key={data.camera_id} position={position} icon={markerIcon} />
         }
         return null
       })}
--- nextjs/src/app/_components/Map/Map.tsx
+++ nextjs/src/app/_components/Map/Map.tsx
@@ -2,6 +2,7 @@
 import { GoogleMap, useJsApiLoader } from "@react-google-maps/api"
 import type { SocrataData } from "~/app/_hooks/useSocrataData"
 import CameraLocationMarker from "./CameraLocationMarker";
+import { useCallback, useState } from "react";
 
 const containerStyle = {
   width: "100vw",
@@ -18,14 +19,32 @@ function Map({ socrataData }: MapProps) {
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY ?? "",
   })
 
+  const [map, setMap] = useState<google.maps.Map | null>(null);
+  const [zoom, setZoom] = useState(17);
+
+  const onMapLoad = useCallback((map: google.maps.Map) => {
+    setMap(map);
+  }, []);
+
+  const handleZoomChanged = () => {
+    if (map) {
+      const newZoom = map.getZoom();
+      if (newZoom) {
+        setZoom(newZoom);
+      }
+    }
+  }
+
   return isLoaded ? (
     <GoogleMap
       mapContainerStyle={containerStyle}
-      zoom={17}
+      zoom={zoom}
       center={new google.maps.LatLng(30.262531, -97.753983)}
       options={{ tilt: 0, mapTypeId: "satellite" }}
+      onLoad={onMapLoad}
+      onZoomChanged={handleZoomChanged}
       >
-        <CameraLocationMarker socrataData={socrataData} />
+        <CameraLocationMarker socrataData={socrataData} zoom={zoom} />
     </GoogleMap>
   ) : (
     <></>
