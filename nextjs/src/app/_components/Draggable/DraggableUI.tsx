9cb971e8d5db52fc892c43fe8367d7fb01b2cc1f
Frank Hereford <github@frankhereford.com>
2025-06-17 22:44:10 +0000

    Update DraggableUI component to enhance styling and functionality
    This commit modifies the DraggableUI component by adjusting the styling properties and enabling aspect ratio locking. The minimum width and height constraints have been commented out, and the GlassMorphism component's properties have been refined for improved visual appeal.

--- nextjs/src/app/_components/Draggable/DraggableUI.tsx
+++ nextjs/src/app/_components/Draggable/DraggableUI.tsx
@@ -17,25 +17,20 @@ export default function DraggableUI() {
     >
       <Rnd
         style={{
-            // backgroundColor: 'white',
-            // border: '1px solid #ccc',
-            // boxShadow: '0 2px 10px rgba(0,0,0,0.1)',
             pointerEvents: 'auto',
             display: 'flex',
             flexDirection: 'column'
         }}
-        default={{
-          x: 20,
-          y: 20,
-          width: 400,
-          height: 300,
-        }}
-        minWidth={200}
-        minHeight={150}
+
+        // minWidth={200}
+        // minHeight={50}
         bounds="parent"
+        lockAspectRatio
+        enableResizing={false}
       >
-        <GlassMorphism>
-          <div style={{ flexGrow: 1, padding: '10px', overflow: 'auto' }}>
+        <GlassMorphism tintColor="#Ffffff" tintOpacity={.1} shadowColor="#fff5"
+          shadowBlur={10} shadowSpread={5} borderRadius={20} outerShadowBlur={4} >
+          <div >
               <h2>Draggable Panel</h2>
               <p>This is a UI panel that you can move around and resize.</p>
               <p>You can put your controls and information here.</p>
