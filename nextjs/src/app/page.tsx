9524b7fb9b988933df8d2890932aa4555e8f5e44
Frank Hereford <github@frankhereford.com>
2025-06-19 16:07:48 +0000

    Add TrafficCameraApplication component to the main page. Integrate it into the layout for improved functionality.

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,15 +1,14 @@
 import Link from "next/link";
 
-
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
-
+import TrafficCameraApplication from "./_components/TrafficCameraApplication";
 
 export default async function Home() {
-
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <TrafficCameraApplication />
       </main>
     </HydrateClient>
   );
