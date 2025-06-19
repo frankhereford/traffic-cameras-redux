6c19bd46819dbae92d047267808aea1a0b45bd89
Frank Hereford <github@frankhereford.com>
2025-06-19 16:14:20 +0000

    Remove loading and error handling UI from TrafficCameraApplication component to streamline the rendering process. The component now directly passes fetched data to the GoogleMap without intermediate loading states.

--- nextjs/src/app/_components/TrafficCameraApplication.tsx
+++ nextjs/src/app/_components/TrafficCameraApplication.tsx
@@ -6,14 +6,6 @@ import useGetSocrataData from "~/app/_hooks/useSocrataData";
 export default function TrafficCameraApplication() {
   const { data: socrataData, isLoading, error } = useGetSocrataData();
 
-//   if (isLoading) {
-//     return <div>Loading traffic camera data...</div>;
-//   }
-
-//   if (error) {
-//     return <div>Error loading traffic camera data: {error.message}</div>;
-//   }
-
   return (
     <>
         <GoogleMap socrataData={socrataData || []}>
