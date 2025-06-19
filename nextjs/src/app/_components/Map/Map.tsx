7acc9676188117b15ec328508ff636b5ff5d57b1
Frank Hereford <github@frankhereford.com>
2025-06-19 16:01:50 +0000

    Add MapView component for displaying Google Maps with default settings. Integrate APIProvider for Google Maps API and set initial camera position and zoom level.

---
+++ nextjs/src/app/_components/Map/Map.tsx
@@ -0,0 +1,44 @@
+"use client";
+import {
+  APIProvider,
+  Map,
+  type MapCameraChangedEvent,
+} from "@vis.gl/react-google-maps";
+import type { SocrataData } from "~/app/_hooks/useSocrataData";
+// import CameraLocationMarkers, { type LatLngBoundsLiteral } from "./CameraLocationMarkers";
+import { useState } from "react";
+
+const containerStyle = {
+  width: "100vw",
+  height: "100vh",
+};
+
+interface MapViewProps {
+  socrataData: SocrataData[];
+}
+
+function MapView({ socrataData }: MapViewProps) {
+  const [zoom, setZoom] = useState(17);
+
+  const position = { lat: 30.262531, lng: -97.753983 };
+
+  return (
+    <APIProvider
+      apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY ?? ""}
+    >
+      <div style={containerStyle}>
+        <Map
+          defaultZoom={zoom}
+          defaultCenter={position}
+          tilt={0}
+          mapId={process.env.NEXT_PUBLIC_GOOGLE_MAP_ID ?? "Traffic-Cameras"}
+          mapTypeId="satellite"
+        >
+
+        </Map>
+      </div>
+    </APIProvider>
+  );
+}
+
+export default MapView;
