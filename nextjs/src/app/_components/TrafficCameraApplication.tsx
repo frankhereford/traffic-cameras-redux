cd4bd4f654aef389d8270a3354ca37f685137b29
Frank Hereford <github@frankhereford.com>
2025-06-19 16:13:54 +0000

    Integrate useGetSocrataData hook into TrafficCameraApplication to fetch traffic camera data. Pass retrieved data to GoogleMap component for enhanced functionality.

--- nextjs/src/app/_components/TrafficCameraApplication.tsx
+++ nextjs/src/app/_components/TrafficCameraApplication.tsx
@@ -1,9 +1,22 @@
+'use client'
+
 import GoogleMap from "./Map/GoogleMap";
+import useGetSocrataData from "~/app/_hooks/useSocrataData";
+
 export default function TrafficCameraApplication() {
+  const { data: socrataData, isLoading, error } = useGetSocrataData();
+
+//   if (isLoading) {
+//     return <div>Loading traffic camera data...</div>;
+//   }
+
+//   if (error) {
+//     return <div>Error loading traffic camera data: {error.message}</div>;
+//   }
+
   return (
     <>
-        <GoogleMap>
-
+        <GoogleMap socrataData={socrataData || []}>
         </GoogleMap>
     </>
   );
