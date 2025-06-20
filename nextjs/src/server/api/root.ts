6eb65456ee4adaf72abf26d4e8869b047ab28d3c
Frank Hereford <github@frankhereford.com>
2025-06-20 00:42:46 +0000

    Refactor and remove unused components related to camera management. Deleted TrafficCamerasApp, CameraImage, DraggableUI, CameraLocationMarkers, and associated hooks and context to streamline the application. Updated Home component to reflect these changes, enhancing overall code organization and reducing complexity.

--- nextjs/src/server/api/root.ts
+++ nextjs/src/server/api/root.ts
@@ -1,5 +1,3 @@
-import { cameraRouter } from "~/server/api/routers/camera";
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -8,8 +6,6 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
-  camera: cameraRouter,
 });
 
 // export type definition of API
