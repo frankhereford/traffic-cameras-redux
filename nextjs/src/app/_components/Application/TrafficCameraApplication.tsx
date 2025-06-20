45385b747383897bbed1a5ee287f1264cdeea3cc
Frank Hereford <github@frankhereford.com>
2025-06-20 03:16:44 +0000

    Refactor TrafficCameraApplication to use activeCameraStore for improved camera data management
    Updated the TrafficCameraApplication component to replace the useCameras hook with useActiveCameraStore, enhancing the management of active camera data. This change streamlines the integration of active camera states into the MapView component.

--- nextjs/src/app/_components/Application/TrafficCameraApplication.tsx
+++ nextjs/src/app/_components/Application/TrafficCameraApplication.tsx
@@ -7,15 +7,15 @@ import MapView from "~/app/_components/Map/Map";
 import useSocrataData from "~/app/_hooks/useSocrataData";
 import useActiveCameras from "~/app/_hooks/useActiveCameras";
 
-import { useCameras } from '~/app/_stores/cameraStore';
+import useActiveCameraStore from '~/app/_stores/activeCameraStore';
 
 export function TrafficCameraApplication() {
   useSocrataData(); // download the data and store it in the cameraStore.
   useActiveCameras(); // maintain a store of active cameras
 
-  const cameras = useCameras();
+  const activeCameras = useActiveCameraStore((state) => state.activeCameras);
 
   return (
-    <MapView socrataData={cameras} /> 
+    <MapView socrataData={activeCameras} /> 
   );
 }
