4df0051afc4ace274146dbaf155aa0618e4020a3
Frank Hereford <github@frankhereford.com>
2025-06-24 18:26:19 +0000

    Add Sidebar component to TrafficCameraApplication for enhanced UI layout

---
+++ nextjs/src/app/_components/Overlay/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="absolute left-0 top-0 z-10 h-screen w-1/5 bg-gray-200/50">
+      {/* We'll add to it. */}
+    </div>
+  );
+};
+
+export default Sidebar;
